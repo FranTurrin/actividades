d75ab993b768eaa320119c4e922b72995e192320
Franco Turrin <francoturrin1540@mail.com>
2024-03-22 20:39:38 -0300

    Agregado de niveles de dificultad

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import random
 # Lista de palabras posibles
-words = ["python", "programación", "computadora", "código", "desarrollo",
+words = ["python", "programacion", "computadora", "codigo", "desarrollo",
 "inteligencia"]
 
 # Elegir una palabra al azar
@@ -15,13 +15,39 @@ failures = 0
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 
+letters = []
+
+# Lista de vocales
+vocales = ["a","e","i","o","u"]
+
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 
-word_displayed = "_" * len(secret_word)
-# Mostrarla palabra parcialmente adivinada
-print(f"Palabra: {word_displayed}")
+print("Facil:1 ; Medio:2 ; Difícil:3")
+dificultad = int(input("Elija un nivel de dificultad: "))
 
+# Dificultad dos(se muestra la última y primera letra)
+if dificultad == 2:
+    for letter in secret_word:
+        letters.append("_")
+    letters[0] = secret_word[0]             
+    letters[-1] = secret_word[-1]
+    word_displayed = "".join(letters)
+    print(f"Palabra : {word_displayed}")
+    guessed_letters.extend([secret_word[0],secret_word[-1]])
+elif dificultad == 1:           # Dificultad uno(se muestran las vocales)
+    guessed_letters.extend(vocales)
+    for letter in secret_word:
+        if letter in guessed_letters:
+            letters.append(letter)
+        else:
+            letters.append("_")
+    word_displayed = "".join(letters)
+    print(f"Palabra : {word_displayed}")
+else:               # Dificultad tres o si se ingreso cualquier otro número(no se muestra nada)
+    word_displayed = "_" * len(secret_word)
+    print(f"Palabra : {word_displayed}")
+        
 while failures != max_failures:
      
      # Pedir al jugador que ingrese una letra
@@ -64,3 +90,5 @@ while failures != max_failures:
 else:
      print(f"¡Oh no! Has agotado tus {max_failures} fallos.")
      print(f"La palabra secreta era: {secret_word}")
+
+# En la dificultad media no puede lograr que por ejemplo en la palabra inteligencia se muestre solo la primer letra "i", se muestran todas en la palabra
